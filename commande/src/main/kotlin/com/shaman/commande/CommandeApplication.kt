a1a81dd62f7a0bd9e2dc3a3fdbc0b7980d13a536
Romain Vetrano <rvetrano@sada.fr>
2020-04-02 08:15:48 +0200

    ajout du commande

---
+++ commande/src/main/kotlin/com/shaman/commande/CommandeApplication.kt
@@ -0,0 +1,11 @@
+package com.shaman.commande
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommandeApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommandeApplication>(*args)
+}
